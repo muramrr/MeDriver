2a8cc87c87adf516ffc9f9ef8271200313fbb007
muramrr <andrii.muramrr@gmail.com>
2020-07-26 19:03:12 +0300

    theme + style skeleton added colors, icon, fonts

--- app/src/main/java/com/mmdev/me/driver/MainActivity.kt
+++ app/src/main/java/com/mmdev/me/driver/MainActivity.kt
@@ -1,7 +1,17 @@
+/*
+ * Created by Andrii Kovalchuk
+ * Copyright (c) 2020. All rights reserved.
+ * Last modified 26.07.20 19:03
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.mmdev.me.driver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {

04140db3c0ea7f131580928d4101e8b3aae96057
muramrr <andrii.muramrr@gmail.com>
2020-07-26 21:29:01 +0300

    added related dependencies

---
+++ app/src/main/java/com/mmdev/me/driver/core/MedriverApp.kt
@@ -0,0 +1,23 @@
+/*
+ * Created by Andrii Kovalchuk
+ * Copyright (c) 2020. All rights reserved.
+ * Last modified 26.07.20 21:29
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.mmdev.me.driver.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *
+ */
+
+@HiltAndroidApp
+class MedriverApp : Application() {
+
+}
--- app/src/main/java/com/mmdev/me/driver/presentation/MainActivity.kt
+++ app/src/main/java/com/mmdev/me/driver/presentation/MainActivity.kt
@@ -1,17 +1,18 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 26.07.20 19:03
+ * Last modified 26.07.20 21:06
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-package com.mmdev.me.driver
+package com.mmdev.me.driver.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mmdev.me.driver.R
 
 class MainActivity: AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {

3f83a5b229e292d10ae43fcbabd0aea89ccbb99c
muramrr <andrii.muramrr@gmail.com>
2020-08-02 20:44:38 +0300

    horizontal issue fixed

--- app/src/main/java/com/mmdev/me/driver/presentation/ui/common/custom/decorators/LinearItemDecoration.kt
+++ app/src/main/java/com/mmdev/me/driver/presentation/ui/common/custom/decorators/LinearItemDecoration.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 29.07.20 18:57
+ * Last modified 02.08.20 19:20
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -18,14 +18,14 @@ import androidx.recyclerview.widget.RecyclerView
  * Linear recyclerView ItemDecoration used to make same gaps between all elements
  */
 
-class LinearItemDecoration: RecyclerView.ItemDecoration() {
+class LinearItemDecoration (private val orientation: Int = 0): RecyclerView.ItemDecoration() {
 
 	override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
 
 		val position = parent.getChildAdapterPosition(view)
 
 		// Add top margin only for the first item to avoid double space between items
-		if (position == 0 ) outRect.top = 30
+		if (position == 0 && orientation == RecyclerView.VERTICAL) outRect.top = 30
 
 		outRect.left = 30
 		outRect.right = 30

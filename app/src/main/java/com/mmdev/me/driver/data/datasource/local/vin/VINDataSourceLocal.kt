7ded9a521872f6c60e4f4d37b24de07a0dd062a6
muramrr <andrii.muramrr@gmail.com>
2020-08-02 16:50:50 +0300

    just in case

---
+++ app/src/main/java/com/mmdev/me/driver/data/datasource/local/vin/VINDataSourceLocal.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Andrii Kovalchuk
+ * Copyright (c) 2020. All rights reserved.
+ * Last modified 02.08.20 16:05
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.mmdev.me.driver.data.datasource.local.vin
+
+/**
+ *
+ */
+

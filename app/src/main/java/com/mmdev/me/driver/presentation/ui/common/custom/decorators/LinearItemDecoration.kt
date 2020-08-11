304f5b67d561d0c8ba820b5921895c3770ae6659
Andrii Kovalchuk <andrii.muramrr@gmail.com>
2020-08-11 21:12:54 +0300

    minor ui fixes

--- app/src/main/java/com/mmdev/me/driver/presentation/ui/common/custom/decorators/LinearItemDecoration.kt
+++ app/src/main/java/com/mmdev/me/driver/presentation/ui/common/custom/decorators/LinearItemDecoration.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 10.08.20 19:21
+ * Last modified 11.08.20 17:56
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -18,7 +18,7 @@ import androidx.recyclerview.widget.RecyclerView
  * Linear recyclerView ItemDecoration used to make same gaps between all elements
  */
 
-class LinearItemDecoration (private val orientation: Int = 0): RecyclerView.ItemDecoration() {
+class LinearItemDecoration (private val orientation: Int = RecyclerView.VERTICAL): RecyclerView.ItemDecoration() {
 
 	override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
 
--- app/src/main/java/com/mmdev/me/driver/presentation/ui/mycar/MyCarFragment.kt
+++ app/src/main/java/com/mmdev/me/driver/presentation/ui/mycar/MyCarFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 11.08.20 15:50
+ * Last modified 11.08.20 17:59
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,6 +10,13 @@
 
 package com.mmdev.me.driver.presentation.ui.mycar
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.lifecycle.Observer
 import com.mmdev.me.driver.R
@@ -22,7 +29,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  *
  */
 
-class MyCarFragment : BaseFlowFragment<MyCarViewModel, FragmentMycarBinding>(R.layout.fragment_mycar) {
+class MyCarFragment : BaseFlowFragment<MyCarViewModel, FragmentMycarBinding>(
+	R.layout.fragment_mycar
+) {
 	
 	
 	data class CarInDropDown (val carBrand: String,
@@ -62,5 +71,24 @@ class MyCarFragment : BaseFlowFragment<MyCarViewModel, FragmentMycarBinding>(R.l
 
 	override fun renderState(state: ViewState) {
 
+	}
+	
+	
+	
+	
+	private class DropAdapter(context: Context, private val data: ArrayList<CarInDropDown>):
+			ArrayAdapter<CarInDropDown>(context, R.layout.item_drop_mycar, data) {
+		
+		override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+			val car: CarInDropDown = getItem(position)
+			val childView : View = convertView ?:
+			                       LayoutInflater.from(context).inflate(R.layout.item_drop_mycar, null)
+			
+			childView.findViewById<TextView>(R.id.tvDropCar).text = car.getFullCarTitle()
+			childView.findViewById<ImageView>(R.id.ivDropCarBrand).setImageResource(car.brandIcon)
+			return childView
+		}
+		
+		override fun getItem(position: Int): CarInDropDown = data[position]
 	}
 }
